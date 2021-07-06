17e745b7c5309dd5ff210c8a01a77f4d45c6d71c
Mauricio Reyes <mreyes@talpor.com>
2021-07-06 15:39:18 -0400

    onetimesecret minor changes

--- contrib/onetimesecret.py
+++ contrib/onetimesecret.py
@@ -1,19 +1,20 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-URL = 'https://onetimesecret.com'
+URL = "https://onetimesecret.com"
 
 
 class OneTimeCli(object):
-
-    def create_link(self, secret):
+    def create_link(self, secret, ttl=900):
         response = requests.post(
-            '{}/api/v1/share'.format(URL),
-            data={'secret': secret, 'ttl': 900},  # live for 15 minutes
-            auth=HTTPBasicAuth(self.user, self.key))
+            "{}/api/v1/share".format(self.url),
+            data={"secret": secret, "ttl": ttl},  # 900 ttl => 15 minutes
+            auth=HTTPBasicAuth(self.user, self.key),
+        )
 
-        return '{}/secret/{}'.format(URL, response.json()['secret_key'])
+        return "{}/secret/{}".format(self.url, response.json()["secret_key"])
 
-    def __init__(self, user, key):
-        self.user = user
+    def __init__(self, user, key, url=URL):
         self.key = key
+        self.url = url
+        self.user = user
